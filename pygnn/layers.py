72662146c0ba6ea2fd5878c59fd5d7cd1f035f25
Navpreet Singh <navpreet_np@yahoo.com>
2021-05-08 17:09:35 -0400

    Initial commit

---
+++ pygnn/layers.py
@@ -0,0 +1,59 @@
+import math
+
+import torch
+
+from torch.nn.parameter import Parameter
+from torch.nn.modules.module import Module
+
+
+class GraphConvolution(Module):
+    """
+    Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
+    """
+
+    def __init__(self, in_features, out_features, bias=True):
+        super(GraphConvolution, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
+        if bias:
+            self.bias = Parameter(torch.FloatTensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, input, adj):
+        support = torch.mm(input, self.weight)
+        output = torch.mm(adj, support)
+        if self.bias is not None:
+            return output + self.bias
+        else:
+            return output
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' \
+               + str(self.in_features) + ' -> ' \
+               + str(self.out_features) + ')'
+
+class InnerProduct(Module):
+
+    def __init__(self, in_dim):
+        super(InnerProduct, self).__init__()
+        self.in_dim = in_dim
+
+    def forward(self, input):
+
+        x,y = torch.split(input,split_size_or_sections=2,dim=1)
+        y = torch.transpose(y,0,1)
+        xy = torch.mm(x,y)
+        xy = torch.flatten(xy)
+        return xy
+
+    def __repr__(self):
+        return self.__class__.__name__
---
+++ pygnn/train.py
@@ -0,0 +1,101 @@
+from __future__ import division
+from __future__ import print_function
+
+import time
+import argparse
+import numpy as np
+
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+
+from utils import load_data,normalize,toy_data
+from models import GNN
+
+# Training settings
+parser = argparse.ArgumentParser()
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='Disables CUDA training.')
+parser.add_argument('--fastmode', action='store_true', default=False,
+                    help='Validate during training pass.')
+parser.add_argument('--seed', type=int, default=426, help='Random seed.')
+parser.add_argument('--epochs', type=int, default=200,
+                    help='Number of epochs to train.')
+parser.add_argument('--lr', type=float, default=0.01,
+                    help='Initial learning rate.')
+parser.add_argument('--weight_decay', type=float, default=5e-4,
+                    help='Weight decay (L2 loss on parameters).')
+parser.add_argument('--hidden', type=int, default=16,
+                    help='Number of hidden units.')
+parser.add_argument('--dropout', type=float, default=0.,
+                    help='Dropout rate (1 - keep probability).')
+parser.add_argument('--ndim', type=int, default=2,
+                    help='Embeddings dimension.')
+
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+# Load data
+#adj = load_data()
+adj = toy_data()
+adj_norm = normalize(adj)
+features = np.identity(adj.shape[0])
+
+adj = torch.FloatTensor(np.array(adj))
+features = torch.FloatTensor(np.array(features))
+adj_norm = torch.FloatTensor(np.array(adj_norm))
+
+
+# Model and optimizer
+model = GNN(nfeat=features.shape[1],
+            nhid=args.hidden,
+            ndim=2*args.ndim,
+            dropout=args.dropout)
+
+activation = {}
+def get_activation(name):
+    def hook(model, input, output):
+        activation[name] = output.detach()
+    return hook
+
+model.reconstructions.register_forward_hook(get_activation('reconstructions'))
+
+optimizer = optim.Adam(model.parameters(),
+                       lr=args.lr, weight_decay=args.weight_decay)
+
+if args.cuda:
+    model.cuda()
+    features = features.cuda()
+    adj = adj.cuda()
+    adj_norm = adj_norm.cuda()
+
+
+def train(epoch):
+
+    t = time.time()
+    model.train()
+    optimizer.zero_grad()
+    output = model(features, adj_norm)
+    criterion = torch.nn.MSELoss()
+    loss = criterion(torch.flatten(output), torch.flatten(adj_norm))
+    loss.backward()
+    optimizer.step()
+
+
+    print('Epoch: {:04d}'.format(epoch+1),
+          'loss: {:.4f}'.format(loss.item()),
+          'time: {:.4f}s'.format(time.time() - t))
+
+
+# Train model
+t_total = time.time()
+for epoch in range(args.epochs):
+    train(epoch)
+print("Optimization Finished!")
+print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
+
